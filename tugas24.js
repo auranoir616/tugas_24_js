b48780f010b854fd8cebf6f4a2fe6cf71f71cfac
febri <fitrafilthmongers@gmail.com>
2023-07-19 13:16:31 +0700

    tugas 24
    mengumpulkan tugas 24 js

---
+++ tugas24.js
@@ -0,0 +1,6 @@
+var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("sebelumnya :",angka.toString())
+
+var angka2 = angka.sort();
+console.log("ascending :",angka2.toString())
+console.log("descending :",angka2.reverse().toString())
